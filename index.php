9da8949e9af3eba39562d8479ec20ba3151e97d1
nasr tech <nasrbilisim@gmail.com>
2019-02-09 00:58:54 +0300

    form post the same page and valiation required field

--- index.php
+++ index.php
@@ -24,12 +24,14 @@
                     <div class="panel-heading"> Form Validation </div>
                     <div class="panel-body">
                         <form method="POST" action="<?php $_SERVER["PHP_SELF"]; ?>">
+                            <p style="color:red;"> Required Field * </p>
                             <div class="form-group">
                                 <label for="name"> Name: </label>
                                 <input type="name" id="name" name="name" class="form-control"/>
                             </div>
                             <div class="form-group">
                                 <label for="email"> E-mail: </label>
+                                <span style="color:red;"> * </span> 
                                 <input type="email" name="email" id="email" class="form-control"/>
                             </div>
                             <div class="form-group">
@@ -38,6 +40,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="pwd"> Password: </label>
+                                <span style="color:red;"> * </span> 
                                 <input type="password" id="pwd" name="password" class="form-control"/>
                             </div>
                             <div class="checkbox">
@@ -52,6 +55,19 @@
                 <div class="panel panel-danger">
                     <div class="panel-heading"> Validation Notes </div>
                     <div class="panel-body">
+                    <ul>
+                    <?php 
+                        if($_POST)
+                        {
+                            if($_POST["email"] == ""){
+                                echo "<li style='color:red;'> email field cannot pass empty </li>";
+                            }
+                            if($_POST["password"] == ""){
+                                echo "<li style='color:red;'> password field cannot pass empty </li>";
+                            }
+                        }
+                    ?>
+                    </ul>
                     </div>
                 </div>
             </div>
